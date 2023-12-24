abab289a4df5908bcc6e9f20edb06d45fda146cc
h4lllllll <hjod1172@yahoo.co.jp>
2023-12-24 08:54:02 +0000

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+[ReactとTypeScriptで2chみたいな掲示板を作る ](https://techbookfest.org/product/uGAcppti6jMYVaKZSKjSLS?productVariantID=8Ee4JmqUGuhgD0f6fBzaW5)を写経するリポジトリ
+
+# dif
+
+## devcontainer
+devcontainerを使う
+## json-serverをnodeで動かす
+devcontainer環境だからなのか、
+- nodemon経由だとAPI叩くところでたまにエラーになる
+- なぜかたまにdb.jsonが空になってしまう
+という問題があった。直接`json-server`を起動するようにしたら大丈夫になった
+
+## biome
+
+## 画面
+
+## suspense / Tanstack Query
---
+++ frontend/vite.config.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+import { join, resolve } from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
+
+const root = resolve(__dirname, "../");
+const tempPortFilePath = join(root, "temp-port.txt");
+
+function getBackendPort() {
+	try {
+		const port = fs.readFileSync(tempPortFilePath, "utf8");
+		console.info(`proxy port: ${port}`);
+		return port.trim(); // 余分な空白や改行を削除
+	} catch (e) {
+		console.error(e);
+		console.warn(
+			`Cannot read port from ${tempPortFilePath}, defaulting to 8062`,
+		);
+		return "8062"; // デフォルト値を設定
+	}
+}
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+	const backendPort = getBackendPort();
+	return defineConfig({
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/api": {
+					target: `http:localhost:${backendPort}`,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ""),
+				},
+			},
+		},
+	});
+};
